e6800a954f2242409ac144c40c2b2f91e23b0fc5
sjain238 <sjain238@asu.edu>
2023-07-15 18:57:26 -0700

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,27 +134,27 @@
     }
   });
 
-  /**
-   * Preloader
-   */
-  let preloader = select("#preloader");
-  if (preloader) {
-    window.addEventListener("load", () => {
-      preloader.remove();
-    });
-  }
+  // /**
+  //  * Preloader
+  //  */
+  // let preloader = select("#preloader");
+  // if (preloader) {
+  //   window.addEventListener("load", () => {
+  //     preloader.remove();
+  //   });
+  // }
 
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
-  });
+  // /**
+  //  * Animation on scroll
+  //  */
+  // window.addEventListener("load", () => {
+  //   AOS.init({
+  //     duration: 1000,
+  //     easing: "ease-in-out",
+  //     once: true,
+  //     mirror: false,
+  //   });
+  // });
 
   /**
    * Initiate Pure Counter
